05f3523afcd7ac108f874341c2066858dc83ce07
Robbie Holmes <robbiethegeek@gmail.com>
2015-04-02 09:36:05 -0400

    Added tests for all the API methods and reorderd the methods and the tests to make them easier to find.

--- lib/idtbeyond/services/imtu.rb
+++ lib/idtbeyond/services/imtu.rb
@@ -4,6 +4,7 @@ require "json"
 module IDTBeyond
   class Service
     class IATU
+      # Constructor
       def initialize(app_id = nil, app_key = nil, term_id = nil)
         raise "app_id, app_key and term_id are required" if app_key.nil? || app_id.nil? || term_id.nil?
         @app_id = app_id
@@ -19,29 +20,21 @@ module IDTBeyond
           faraday.response :logger
           faraday.adapter Faraday.default_adapter
         end
-
       end
 
-      def get_app_id
-        @app_id
-      end
+      # API Methods
 
-      def get_app_key
-        @app_key
+      def client_transaction_id_search(client_transaction_id, date_from, date_to)
+        response = @conn.post "/v1/iatu/topups/reports", {
+        :client_transaction_id => client_transaction_id,
+        :type_of_report => 'client_transaction_id',
+        :date_from => date_from,
+        :date_to => date_to
+        }
+        JSON response.body
       end
-      def get_status
-        response = @conn.get "/v1/status"
-        p response
-        return JSON response.body if response.status == 200
-        false
-      end
-      def get_balance
-        response = @conn.get "/v1/iatu/balance"
-        return JSON response.body if response.status == 200
-        false
-      end
-      def get_all_topups_totals(date_from, date_to)
-        response = @conn.get "/v1/iatu/topups/reports/totals", {:date_from => date_from, :date_to => date_to}
+      def get_all_charges(date_from, date_to)
+        response = @conn.get "/v1/iatu/charges/reports/all", {:date_from => date_from, :date_to => date_to}
         return JSON response.body if response.status == 200
         false
       end
@@ -50,59 +43,70 @@ module IDTBeyond
         return JSON response.body if response.status == 200
         false
       end
-      def get_all_charges(date_from, date_to)
-        response = @conn.get "/v1/iatu/charges/reports/all", {:date_from => date_from, :date_to => date_to}
+      def get_all_topups_totals(date_from, date_to)
+        response = @conn.get "/v1/iatu/topups/reports/totals", {:date_from => date_from, :date_to => date_to}
+        return JSON response.body if response.status == 200
+        false
+      end
+      def get_app_id
+        @app_id
+      end
+
+      def get_app_key
+        @app_key
+      end
+      def get_balance
+        response = @conn.get "/v1/iatu/balance"
         return JSON response.body if response.status == 200
         false
       end
       def get_local_value(amount, carrier_code, country_code, currency_code)
         response = @conn.get "/v1/iatu/products/reports/local-value", {
-          :carrier_code => carrier_code,
-          :country_code => country_code,
-          :currency_code => currency_code,
-          :amount => amount
+        :carrier_code => carrier_code,
+        :country_code => country_code,
+        :currency_code => currency_code,
+        :amount => amount
         }
         JSON response.body
       end
-      def validate_number(country_code, phone_number)
-        response = @conn.get "/v1/iatu/number-validator", {:country_code => country_code, :mobile_number => phone_number}
-        JSON response.body
+
+      def get_status
+        response = @conn.get "/v1/status"
+        p response
+        return JSON response.body if response.status == 200
+        false
       end
 
       def post_topup(amount, carrier_code, country_code, phone_number )
         client_transaction_id = @app_id + '-' + "%06d" % Random.rand(0..999999)
         response = @conn.post "/v1/iatu/topups", {
-         :country_code => country_code,
-         :carrier_code => carrier_code,
-         :client_transaction_id => client_transaction_id,
-         :terminal_id => @term_id,
-         :mobile_number => phone_number,
-         :plan => @plan_type,
-         :amount => amount
-        }
-        JSON response.body
-      end
-      def client_transaction_id_search(client_transaction_id, date_from, date_to)
-        response = @conn.post "/v1/iatu/topups/reports", {
-          :client_transaction_id => client_transaction_id,
-          :type_of_report => 'client_transaction_id',
-          :date_from => date_from,
-          :date_to => date_to
-        }
-        JSON response.body
-      end
-      def to_service_number_search(to_service_number)
-        response = @conn.post "/v1/iatu/topups/reports", {
-          :to_service_number => to_service_number,
-          :type_of_report => 'to_service_number'
+        :country_code => country_code,
+        :carrier_code => carrier_code,
+        :client_transaction_id => client_transaction_id,
+        :terminal_id => @term_id,
+        :mobile_number => phone_number,
+        :plan => @plan_type,
+        :amount => amount
         }
         JSON response.body
       end
       def reverse_topup(client_transaction_id, to_service_number)
         response = @conn.post "/v1/iatu/topups/reverse", {
-          :client_transaction_id => client_transaction_id,
-          :to_service_number => to_service_number
+                                                           :client_transaction_id => client_transaction_id,
+                                                           :to_service_number => to_service_number
+                                                       }
+        JSON response.body
+      end
+      def to_service_number_search(to_service_number)
+        response = @conn.post "/v1/iatu/topups/reports", {
+        :to_service_number => to_service_number,
+        :type_of_report => 'to_service_number'
         }
+        JSON response.body
+      end
+      def validate_number(country_code, phone_number)
+        response = @conn.get "/v1/iatu/number-validator", {
+                                                            :country_code => country_code, :mobile_number => phone_number}
         JSON response.body
       end
     end
--- spec/imtu_spec.rb
+++ spec/imtu_spec.rb
@@ -5,6 +5,80 @@ describe 'IDTBeyond - IMTU' do
     @idtbeyond = IDTBeyond::Service::IATU.new 'app-id', 'app-key', 'test term id'
 
   end
+  describe 'client_transaction_id_search' do
+    it 'should call the /v1/iatu/topups/reports' do
+      stub_request(:post, "https://api.idtbeyond.com/v1/iatu/topups/reports").
+          with(
+              :body => {
+                  "client_transaction_id"=>"client-transaction-id",
+                  "date_from" => "date-from",
+                  "date_to" => "date-to",
+                  "type_of_report"=>"client_transaction_id" },
+              :headers => {
+                  'Accept'=>'*/*',
+                  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                  'Content-Type'=>'application/x-www-form-urlencoded',
+                  'User-Agent'=>'Faraday v0.9.1',
+                  'X-Idt-Beyond-App-Id'=>'app-id',
+                  'X-Idt-Beyond-App-Key'=>'app-key'}).
+          to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      response = @idtbeyond.client_transaction_id_search('client-transaction-id', 'date-from', 'date-to')
+      expect(response['success']).to eq(true)
+    end
+  end
+  describe 'get_all_charges' do
+    it 'should call the /v1/iatu/charges/reports/all' do
+      stub_request(
+      :get,
+      "https://api.idtbeyond.com/v1/iatu/charges/reports/all?date_from=date-from&date_to=date-to").
+      with(
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+
+      response = @idtbeyond.get_all_charges("date-from", "date-to")
+      expect(response['success']).to eq(true)
+    end
+  end
+  describe 'get_all_topups' do
+    it 'should call the /v1/iatu/topups/reports/all' do
+      stub_request(
+      :get,
+      "https://api.idtbeyond.com/v1/iatu/topups/reports/all?date_from=date-from&date_to=date-to").
+      with(
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      response = @idtbeyond.get_all_topups("date-from", "date-to")
+      expect(response['success']).to eq(true)
+    end
+  end
+  describe 'get_all_topups_totals' do
+    it 'should call the /v1/iatu/topups/reports/totals' do
+      stub_request(
+      :get,
+      "https://api.idtbeyond.com/v1/iatu/topups/reports/totals?date_from=date-from&date_to=date-to").
+      with(
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+
+      response = @idtbeyond.get_all_topups_totals("date-from", "date-to")
+      expect(response['success']).to eq(true)
+    end
+  end
   describe 'get_app_id' do
     it 'returns the app id that was passed in during instantiation ' do
       expect(@idtbeyond.get_app_id).to eq('app-id')
@@ -15,16 +89,49 @@ describe 'IDTBeyond - IMTU' do
       expect(@idtbeyond.get_app_key).to eq('app-key')
     end
   end
+  describe 'get_balance' do
+    it 'should call the /v1/iatu/balance' do
+      stub_request(:get, "https://api.idtbeyond.com/v1/iatu/balance").
+      with(
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+
+      response = @idtbeyond.get_balance
+      expect(response['success']).to eq(true)
+    end
+  end
+  describe 'get_local_value' do
+    it 'should call the /v1/iatu/number-validator' do
+      stub_request(
+      :get,
+      "https://api.idtbeyond.com/v1/iatu/products/reports/local-value?amount=amount&carrier_code=carrier-code&country_code=country-code&currency_code=currency-code").
+      with(
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      response = @idtbeyond.get_local_value('amount', 'carrier-code', 'country-code', 'currency-code')
+      expect(response['success']).to eq(true)
+    end
+  end
   describe 'get_status' do
     it 'should call the /v1/status' do
       stub_request(:get, "https://api.idtbeyond.com/v1/status").
-          with(:headers => {
-            'Accept'=>'*/*',
-            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'User-Agent'=>'Faraday v0.9.1',
-            'X-Idt-Beyond-App-Id'=>'app-id',
-            'X-Idt-Beyond-App-Key'=>'app-key'}).
-          to_return(:status => 200, :body => "{\"alive\":true}", :headers => {})
+      with(:headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"alive\":true}", :headers => {})
       response = @idtbeyond.get_status
       expect(response['alive']).to eq(true)
     end
@@ -32,23 +139,23 @@ describe 'IDTBeyond - IMTU' do
   describe 'post_topup' do
     it 'should call the /v1/iatu/topups' do
       stub_request(:post, "https://api.idtbeyond.com/v1/iatu/topups").
-          with(
-            :body => {
-              "amount"=>"amount",
-              "carrier_code"=>"carrier-code",
-              "client_transaction_id"=>/app-id-\d+/,
-              "country_code"=>"country-code",
-              "mobile_number"=>"phone-number",
-              "plan"=>"Production",
-              "terminal_id"=>"test term id"},
-            :headers => {
-              'Accept'=>'*/*',
-              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Content-Type'=>'application/x-www-form-urlencoded',
-              'User-Agent'=>'Faraday v0.9.1',
-              'X-Idt-Beyond-App-Id'=>'app-id',
-              'X-Idt-Beyond-App-Key'=>'app-key'}).
-          to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      with(
+      :body => {
+    "amount"=>"amount",
+    "carrier_code"=>"carrier-code",
+    "client_transaction_id"=>/app-id-\d+/,
+    "country_code"=>"country-code",
+    "mobile_number"=>"phone-number",
+    "plan"=>"Production",
+    "terminal_id"=>"test term id"},
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'Content-Type'=>'application/x-www-form-urlencoded',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
       response = @idtbeyond.post_topup "amount", "carrier-code", "country-code", "phone-number"
       expect(response['success']).to eq(true)
     end
@@ -56,19 +163,55 @@ describe 'IDTBeyond - IMTU' do
   describe 'reverse_topup' do
     it 'should call the /v1/iatu/topups/reverse' do
       stub_request(:post, "https://api.idtbeyond.com/v1/iatu/topups/reverse").
-          with(
-            :body => {
-              "client_transaction_id"=>"client-transaction-id",
-              "to_service_number"=>"to-service-number"},
-            :headers => {
-              'Accept'=>'*/*',
-              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Content-Type'=>'application/x-www-form-urlencoded',
-              'User-Agent'=>'Faraday v0.9.1',
-              'X-Idt-Beyond-App-Id'=>'app-id',
-              'X-Idt-Beyond-App-Key'=>'app-key'}).
-          to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      with(
+      :body => {
+    "client_transaction_id"=>"client-transaction-id",
+    "to_service_number"=>"to-service-number"},
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'Content-Type'=>'application/x-www-form-urlencoded',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
       response = @idtbeyond.reverse_topup "client-transaction-id", "to-service-number"
+      expect(response['success']).to eq(true)
+    end
+  end
+  describe 'to_service_number_search' do
+    it 'should call the /v1/iatu/topups/reports' do
+      stub_request(:post, "https://api.idtbeyond.com/v1/iatu/topups/reports").
+      with(
+      :body => {
+    "to_service_number"=>"to-service-number",
+    "type_of_report"=>"to_service_number" },
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'Content-Type'=>'application/x-www-form-urlencoded',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      response = @idtbeyond.to_service_number_search('to-service-number')
+      expect(response['success']).to eq(true)
+    end
+  end
+  describe 'validate_number' do
+    it 'should call the /v1/iatu/number-validator' do
+      stub_request(
+      :get,
+      "https://api.idtbeyond.com/v1/iatu/number-validator?country_code=country-code&mobile_number=phone-number").
+      with(
+      :headers => {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.9.1',
+    'X-Idt-Beyond-App-Id'=>'app-id',
+    'X-Idt-Beyond-App-Key'=>'app-key'}).
+      to_return(:status => 200, :body => "{\"success\":true}", :headers => {})
+      response = @idtbeyond.validate_number('country-code', 'phone-number')
       expect(response['success']).to eq(true)
     end
   end
